82c52a4819eb294f95cfa4a8477ca037d622ec10
Gabriel Alexsandro <gabrielalexsandro2011@gmail.com>
2022-06-16 12:13:35 -0300

    Else if on argument validation comparation

--- src/unknownProfile.cpp
+++ src/unknownProfile.cpp
@@ -2,7 +2,7 @@
 
 UnknownProfile::UnknownProfile(int argc, char *argv[]) {
     if(isExecutionArgumentsValids(argc, argv))
-        cout << "> Loading the DNA database [" << "data_file_name" << "]..." << endl;
+        cout << "> Loading the DNA database [" << data_file_name << "]..." << endl;
     else
         cout << "Error with arguments in execution." << endl;
 }
@@ -13,7 +13,7 @@ bool UnknownProfile::isExecutionArgumentsValids(int argc, char *argv[]) {
         for(int i=0; i < argc - 1; i++) {
             if(argv[i] == DATABASE_ARGUMENT_NAME)
                 isDatabaseArgumentNameFound = true;
-            if(argv[i] == SEQUENCE_ARGUMENT_NAME)
+            else if(argv[i] == SEQUENCE_ARGUMENT_NAME)
                 isSequenceArgumentNameFound = true;
         }
     }
